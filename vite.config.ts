1687c827e3da20aa9c7dabefa68a305d0d3aa3de
Swapnil Rajput <rajput.swapnil3090@gmail.com>
2026-01-15 21:53:24 +0530

    define APi key in vite.config.js file

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 
   // IMPORTANT: must match GitHub repo name exactly
   base: '/Stealth-Copilot/',
-
+  define: { 'process.env.API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY), 'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY) },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
